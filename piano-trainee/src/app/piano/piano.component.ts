024a48a51ed453615e4b658920e8b61df9686d9f
yugohg <yugiharagg.yh@gmail.com>
2021-09-06 00:44:36 -0300

    Editing Piano size

--- piano-trainee/src/app/piano/piano.component.ts
+++ piano-trainee/src/app/piano/piano.component.ts
@@ -7,13 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PianoComponent implements OnInit {
 
+	public keyWidth = 77;
+	public keyHeight = 380;
+	public octaveWidth: number = 6.5*this.keyWidth;
+
 	public startOctave: number = 4;
 	public octaves: number = 1;
+	private zoom: number = 0;
 
 	public notes = ['c','c#','d','d#','e','f','f#','g','g#','a','a#','b']
 
 	constructor() { }
 
 	ngOnInit(): void {
+	}
+
+	public zoomIn(): void {
+		this.zoom += 1;
+		if(this.zoom){
+			this.keyWidth = 77*(this.zoom + 0.1);
+		}
+	}
+
+	public zoomOut(): void {
+		this.zoom -= 1;
+		if(this.zoom){
+			this.keyWidth = 77*(this.zoom + 0.1);
+		}
 	}
 }
